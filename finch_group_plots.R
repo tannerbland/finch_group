071d8f75d5af0abacb9c5c12a4a38c619900c25a
tannerbland <tannerbland@arizona.edu>
2024-09-24 10:23:13 -0700

    uploaded file for plots :) -tanner

---
+++ finch_group_plots.R
@@ -0,0 +1,14 @@
+### Finch.csv Group
+### ECOL596W McNew
+### 9/24/2024
+### Gena, Yinpeng, Tanner
+
+library(dplyr)
+library(ggplot2)
+
+finches <- read.csv('finch.csv')
+
+# clean dataset by removing NAs and grouping by 
+# important variables
+
+finches_sp <- select()
